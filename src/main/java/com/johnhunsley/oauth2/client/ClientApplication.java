673a28803dc2d163f63fdeda4cf85f4b99698736
U0181792 <john.hunsley@thomsonreuters.com>
2018-11-08 16:50:11 +0000

    basic thymleaf OAuth2 client

--- src/main/java/com/johnhunsley/oauth2/client/ClientApplication.java
+++ src/main/java/com/johnhunsley/oauth2/client/ClientApplication.java
@@ -1,23 +1,21 @@
 package com.johnhunsley.oauth2.client;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.context.request.RequestContextListener;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.web.context.request.RequestContextListener;
 
-public class ClientApplication {
+@SpringBootApplication
+public class ClientApplication extends SpringBootServletInitializer {
 
-    @SpringBootApplication
-    public class UiApplication extends SpringBootServletInitializer {
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
+    }
 
-        @Bean
-        public RequestContextListener requestContextListener() {
-            return new RequestContextListener();
-        }
-
-        public static void main(String[] args) {
-            SpringApplication.run(ClientApplication.class, args);
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
     }
 }
+
